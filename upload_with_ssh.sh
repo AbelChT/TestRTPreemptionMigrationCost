a426ad8f8bb6ced04a1e31d69fabc754d91fdce8
AbelChT <abelcht@outlook.es>
2021-02-23 18:31:22 +0100

    improved commands

--- upload_with_ssh.sh
+++ upload_with_ssh.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # No print version
-scp ./builds/{main_continuous,main_migration,main_preemption} root@192.168.2.2:/root/
+scp ./builds/aarch64/{main_continuous,main_migration,main_preemption} root@192.168.2.2:/root/
 
 # Print version
-scp ./builds/{main_continuous_print,main_migration_print,main_preemption_print} root@192.168.2.2:/root/
+scp ./builds/aarch64/{main_continuous_print,main_migration_print,main_preemption_print} root@192.168.2.2:/root/
